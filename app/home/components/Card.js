05c414d6da39c3eeb4f462b766710866f44328b7
velho-comercial <velho.comercial@gmail.com>
2025-03-26 21:22:53 -0300

    grayscale

--- app/home/components/Card.js
+++ app/home/components/Card.js
@@ -1,8 +1,10 @@
 import styles from './Card.module.css';
 
 export default function Card(props) {
+    const grayscaleStyle = props.disponivel ? {} : { filter: 'grayscale(100%)' };
+
     return (
-        <div className={styles.card}>
+        <div className={styles.card} style={grayscaleStyle}>
             <div className={ props.disponivel ? styles.txt : styles.txtLine }>{ props.sabor }</div> 
              <img className={styles.img} src="pasteis.jpg"></img>
             <div className={styles.txtSmall}>{props.preco}</div> 
